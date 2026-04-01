25c179728ffff3b62a4744adc86650d734e9349b
evecus <jamie55@163.com>
2026-04-01 18:30:33 +0800

    Delete frontend directory

--- frontend/src/components/ToastMsg.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="toast" :class="{ show: visible }">{{ message }}</div>
-</template>
-
-<script setup>
-import { useToast } from '../composables/useToast.js'
-const { message, visible } = useToast()
-</script>
